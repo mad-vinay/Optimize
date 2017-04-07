fb73726fb1f47844f2a348cad3564f4c1dd87d7e
geethuj <geethuj@qburst.com>
2017-04-07 16:29:53 +0530

    Style fix in login page:

---
+++ src/components/TextBox/style.js
@@ -0,0 +1,26 @@
+/**
+ * Info Style sheet
+ */
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+
+    loginform: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        paddingRight: 5,
+        paddingLeft: 5,
+        borderColor: '#748d92',
+        borderRadius: 10,
+        borderWidth: 1,
+    },
+    form_items: {
+        alignSelf: 'stretch',
+        color: '#fff',
+        borderColor: '#748d92',
+        borderRadius: 5,
+        height:42,
+        width:230,
+    },
+});
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -3,7 +3,7 @@ import Container from '../../components/Container';
 import Button from '../../components/Button';
 import Ticker from '../../components/Ticker';
 import NavBar from '../../components/NavBar';
-
+import TextBox from '../../components/TextBox';
 
 import {
   StyleSheet,
@@ -30,31 +30,16 @@ export default class Login extends Component {
 							source={require('../../images/Optimize-logo.png')}
 				        />
 				     </View>
-			        <Container style={styles.containerstyle}>
-					    <TextInput
-					        style={styles.textInput}
-					        placeholder = 'Email'
-					        underlineColorAndroid='transparent'
-					        placeholderTextColor="grey"
-					    />
-					    <View style={styles.remembermeTextWrap}>
+				     <TextBox placeholder = 'Email'/>
+				     <View style={styles.remembermeTextWrap}>
 						    <Text style={styles.rememberText}>Remember Email</Text>
 						    <Ticker/>
 						</View>
-					</Container>
-					<Container>
-					    <TextInput
-					        secureTextEntry={true}
-					        style={styles.textInput}
-					        placeholder = 'Password'
-					        underlineColorAndroid='transparent'
-					        placeholderTextColor="grey"
-					    />
-					    <View style={styles.remembermeTextWrap}>
+				     <TextBox placeholder = 'Password'/>
+				     <View style={styles.remembermeTextWrap}>
 					    	<Text style={styles.rememberText}>Remember Password</Text>
 					        <Ticker/>
 						</View>
-					</Container>
 					<Container>
 					    <Button
 					        styles={styles.transparentButton}
